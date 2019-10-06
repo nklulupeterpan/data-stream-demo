edcbf4e6b16cf0fe8fe48409d61f012d1f8e2d73
nklulupeterpan <yanglunk@gmail.com>
2019-10-06 10:13:31 +0100

    Started hsqlserver

--- src/main/java/com/event/example/demo/DemoApplication.java
+++ src/main/java/com/event/example/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.event.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 
 @SpringBootApplication
+@ImportResource(value="classpath:/hsql_cfg.xml")
 public class DemoApplication {
 
     public static void main(String[] args) {
